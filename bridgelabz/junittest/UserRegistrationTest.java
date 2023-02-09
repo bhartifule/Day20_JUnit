650a84ccde42a4e268691014f164304c608e02cd
Bharti <bharatifule10gmail.com>
2023-02-09 23:48:58 +0530

    Added: Junit Problems

--- bridgelabz/junittest/UserRegistrationTest.java
+++ bridgelabz/junittest/UserRegistrationTest.java
@@ -8,52 +8,64 @@ import com.bridglabz.junt.UserRegistration;
 
 public class UserRegistrationTest {
 	static UserRegistration userRegistration;
+    @BeforeAll
+    public static void init(){
+        userRegistration = new UserRegistration();
+    }
+    @Test
+    public void firstNmeShouldBeTrue(){
+        boolean value = userRegistration.checkFirstName("Bharti");
+        Assertions.assertEquals(true , value);
+    }
+    @Test
+    public void firstNmeShouldBeFalse(){
+        boolean value = userRegistration.checkFirstName("Bharti");
+        Assertions.assertEquals(false , value);
+    }
+    @Test
+    public void lastNameTest2_ShouldReturnTrue() {
+        boolean value = userRegistration.checkLastName("Fule");
+        Assertions.assertEquals(true, value);
+    }
+    @Test
+    public void lastNameTest2_ShouldReturnFalse() {
+        boolean value = userRegistration.checkLastName("Fule");
+        Assertions.assertEquals(false, value);
+    }
 
-	@BeforeAll
-	public static void init() {
-		userRegistration = new UserRegistration();
-	}
+    @Test
+    public void emailTest3_ShouldReturnTrue() {
+        boolean value = userRegistration.checkEmail("Bharatifule@gmail.com");
+        Assertions.assertEquals(true, value);
+    }
+    @Test
+    public void emailTest3_ShouldReturnFalse() {
+        boolean value = userRegistration.checkEmail("bhartifule@gmail.com");
+        Assertions.assertEquals(false, value);
+    }
 
-	@Test
-	public void firstNmeShouldBeTrue() {
-		boolean value = userRegistration.checkFirstName("Bharti");
-		Assertions.assertEquals(true, value);
-	}
+    @Test
+    public void numberTest4_ShouldReturnTrue() {
+        boolean value = userRegistration.phoneNumber("91 9956561212");
+        Assertions.assertEquals(true, value);
+    }
+    @Test
+    public void numberTest4_ShouldReturnFalse() {
+        boolean value = userRegistration.phoneNumber("918958861212");
+        Assertions.assertEquals(false, value);
+    }
 
-	public void firstNmeShouldBeFalse() {
-		boolean value = userRegistration.checkFirstName("Fule");
-		Assertions.assertEquals(false, value);
-	}
+    @Test
+    public void passWordTest5_ShouldReturnTrue() {
+        boolean value = userRegistration.passWord("Bharti@123");
+        Assertions.assertEquals(true ,value);
+    }
+    @Test
+    public void passWordTest5_ShouldReturnFalse() {
+        boolean value = userRegistration.passWord("Bharti@123");
+        Assertions.assertEquals(false ,value);
+    }
 
-	@Test
-	public void lastNameTest2_ShouldReturnTrue() {
-		boolean value = userRegistration.checkLastName("Fule");
-		Assertions.assertEquals(true, value);
-	}
 
-	@Test
-	public void emailTest3_ShouldReturnTrue() {
-		boolean value = userRegistration.checkEmail("bhartifule24@gmail.com");
-		Assertions.assertEquals(true, value);
-	}
-
-	@Test
-	public void numberTest4_ShouldReturnTrue() {
-		boolean value = userRegistration.phoneNumber("91 8856561212");
-		Assertions.assertEquals(true, value);
-	}
-
-	@Test
-	public void passWordTest5_ShouldReturnTrue() {
-		boolean value = userRegistration.passWord("bharti@123");
-		Assertions.assertEquals(true, value);
-	}
-
-	@Test
-	public void testMobileNumberHappy() {
-		UserRegistration userValidator = new UserRegistration();
-		boolean isValid = userValidator.phoneNumber("91 9984982171");
-		Assertions.assertEquals(true, isValid);
-	}
 
 }
